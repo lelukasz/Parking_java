5b55f857616cfc12db760b835aa69f76e809a170
Lukasz Leszczynski <leszczynski.lukasz1997@gmail.com>
2022-04-22 17:09:01 +0200

    Test2

---
+++ src/Parking.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parking {
+    private static final int number_of_places = 350;
+    private List<ParkingSpace> parkingSpaces = new ArrayList<>();
+}
